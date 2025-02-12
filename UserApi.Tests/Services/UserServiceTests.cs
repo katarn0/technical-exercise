ab6e057fb13ff0103a6cfca7613a96b0601dd79f
Jesse Phillips <jesse@phillipsnv.com>
2025-02-12 10:12:10 -0800

    Added the current base tests for the UserServiceTests.

--- UserApi.Tests/Services/UserServiceTests.cs
+++ UserApi.Tests/Services/UserServiceTests.cs
@@ -1,12 +1,85 @@
 ﻿namespace WebApi.UserApi.Tests.Services;
 
+using FluentValidation;
+
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+
+using Moq;
+
+using NUnit.Framework;
+
+using Data;
+using Models;
+using Models.Validators;
+using WebApi.UserApi.Services;
 
 [TestFixture]
 public class UserServiceTests
 {
+
+    private UserContext _dbContext;
+    private UserService _userService;
+    private IValidator<User> _userValidator;
+
+
+    [SetUp]
+    public void SetUp()
+    {
+        var options = new DbContextOptionsBuilder<UserContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _dbContext = new UserContext(options);
+        _dbContext.Database.EnsureCreated();
+
+        _userValidator = new UserValidator();
+        _userService = new UserService(_dbContext, _userValidator);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _dbContext.Database.EnsureDeleted();
+        _dbContext.Dispose();
+    }
+    [Test]
+    public async Task AddUserAsync_ShouldAddUser()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public void AddUserAsync_ShouldThrowValidationException_WhenInvalidUser()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task GetUsersAsync_ShouldReturnAllUsers()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task GetUserByIdAsync_ShouldReturnUserIfExists()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task UpdateUserAsync_ShouldUpdateExistingUser()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Test]
+    public async Task DeleteUserAsync_ShouldRemoveUserIfExists()
+    {
+        throw new NotImplementedException();
+    }
+
 }
